a2cba92af15e979ccf78a7285161e235ec7fb591
unknown <sakshikorishetti.xworks@gmail.com>
2023-07-04 12:25:43 +0530

    object

---
+++ Computer.java
@@ -0,0 +1,66 @@
+public class Computer{
+	string computerBrand;
+	int computerColour;
+	int partsOfComputer;
+	int ram;
+	string OperatingSystem;
+	String processor;
+	int speed;
+	int accuracy;
+	String realibility;
+	int memory;
+	int storage;	
+    int height;
+    String TypeOfMouse;
+    int weight;
+    int price;
+    float version;
+	String model;
+	String compatability;
+	String internetConnectivity;
+	int Weight 
+	String ports;
+	
+	public static void main(String[] args)
+	{
+	Computer obj =  new Computer(32,350000);	
+	}
+
+
+public Computer(int ram,int price){
+	this.ram=ram;
+	this.price=price;
+	System.out.println(ram);
+	System.out.println(price);
+	
+}
+
+public static void watching(){
+	
+} 
+public static void browsing(){
+	
+} 
+public static void gaming(){
+	
+} 
+public static void listeningSong(){
+	
+}
+public static void installingApps(){
+	
+}
+public static void print(){
+	
+}
+public static void scan(){
+	
+}
+public static void coding(){
+	
+}
+public static void socialMediaScrolling(){
+	
+}
+
+}
